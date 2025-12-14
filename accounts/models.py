f4e038795c6d7d146b0e78340721a9b0c2375798
sanabinadaf <71943182+ishannadaf@users.noreply.github.com>
2025-12-14 23:19:48 +0530

    Authentication issues

--- accounts/models.py
+++ accounts/models.py
@@ -7,8 +7,12 @@ class User(AbstractUser):
         ('admin', 'Admin'),
         ('watchman', 'Watchman'),
     )
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
 
+    role = models.CharField(
+        max_length=20,
+        choices=ROLE_CHOICES,
+        default='admin'
+    )
 
 
 class ActivityLog(models.Model):
