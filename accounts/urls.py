49c5112644026de21f10c2fad4d7583d9ad03706
sanabinadaf <71943182+ishannadaf@users.noreply.github.com>
2025-12-14 23:15:35 +0530

    Admin signup changes

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import login_view, logout_view, activity_logs
+from .views import login_view, logout_view, activity_logs, admin_signup
 
 urlpatterns = [
     path('', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
     path('logs/', activity_logs, name='activity_logs'),
-
+    path("signup/", admin_signup, name="admin_signup"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,8 @@ from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 from .models import ActivityLog
 from accounts.decorators import role_required
+from django.contrib.auth import get_user_model
+from django.contrib import messages
 
 def login_view(request):
     error = None
@@ -31,3 +33,34 @@ def activity_logs(request):
     return render(request, 'accounts/activity_logs.html', {
         'logs': logs
     })
+    
+
+User = get_user_model()
+    
+def admin_signup(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        confirm = request.POST.get("confirm_password")
+
+        if password != confirm:
+            messages.error(request, "Passwords do not match")
+            return redirect("admin_signup")
+
+        if User.objects.filter(username=username).exists():
+            messages.error(request, "Username already exists")
+            return redirect("admin_signup")
+
+        user = User.objects.create_user(
+            username=username,
+            password=password
+        )
+        user.role = "admin"
+        user.is_staff = True
+        user.is_superuser = True
+        user.save()
+
+        messages.success(request, "Admin account created. Please login.")
+        return redirect("login")
+
+    return render(request, "accounts/admin_signup.html")
